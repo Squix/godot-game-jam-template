e4df51ab9bfb6fc27577f8a261f378838c79d436
SquixTech <6475960+Squix@users.noreply.github.com>
2026-06-12 16:32:47 +0200

    fix: remove debug assets code

--- scripts/audio_player_manager.gd
+++ scripts/audio_player_manager.gd
@@ -3,7 +3,7 @@ class_name AudioPlayerManager
 
 enum SFX {ButtonClick}
 
-var _button_click_sfx_stream : AudioStream = preload("res://assets/sounds/button_click.mp3")
+#var _button_click_sfx_stream : AudioStream = preload("res://assets/sounds/button_click.mp3")
 
 @onready var _sfx_player : AudioStreamPlayer = $SFXplayer
 @onready var _bgm_player : AudioStreamPlayer = $BGMplayer
@@ -12,7 +12,8 @@ func play_sfx(sfx_name: SFX) -> void:
 	_sfx_player.stop()
 	match sfx_name:
 		SFX.ButtonClick:
-			_sfx_player.stream = _button_click_sfx_stream
+			print("TODO: preload button click SFX audio")
+			#_sfx_player.stream = _button_click_sfx_stream
 			
 	_sfx_player.pitch_scale = randf_range(0.7,1.35)
 	_sfx_player.play()
