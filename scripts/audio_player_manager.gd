265ec077601d25f8ec701bc9a6c40e94ad6af856
SquixTech <6475960+Squix@users.noreply.github.com>
2026-06-12 11:21:32 +0200

    feat: audio manager

---
+++ scripts/audio_player_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+class_name AudioPlayerManager
+
+enum SFX {ButtonClick}
+
+var button_click_sfx_stream = preload("res://assets/sounds/button_click.mp3")
+
+@onready var _sfx_player : AudioStreamPlayer = $SFXplayer
+@onready var _bgm_player : AudioStreamPlayer = $BGMplayer
+
+func play_sfx(sfx_name: SFX) -> void:
+	_sfx_player.stop()
+	match sfx_name:
+		SFX.ButtonClick:
+			_sfx_player.stream = button_click_sfx_stream
+			
+	_sfx_player.pitch_scale = randf_range(0.7,1.35)
+	_sfx_player.play()
+	
+func play_bgm() -> void:
+	_bgm_player.play()
+	
+func stop_bgm() -> void:
+	_bgm_player.stop()
--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -2,12 +2,14 @@ extends Control
 class_name UI
 
 signal restart_button_pressed()
-#signal playSFX(sound:SFXplayer.SFX)
+signal play_sfx(sound:AudioPlayerManager.SFX)
 signal setting_changed(setting: SettingsManager.Settings, value: Variant)
 
 @onready var settings_ui : Control = %SettingsDialog
 @onready var game_over_ui : GameOverUI = %GameOver
 @onready var score_label : Label = %ScoreLabel
+
+# game_manager signals handlers
 
 func _on_game_manager_game_won(score: int) -> void:
 	game_over_ui.set_title("Game won!")
@@ -31,18 +33,24 @@ func _on_game_manager_init_game() -> void:
 func _on_game_manager_score_updated(new_score: int) -> void:
 	score_label.text = "{0}".format([new_score])
 
+# children buttons
+
 func _on_restart_button_pressed() -> void:
-	#playSFX.emit(SFXplayer.SFX.ButtonClick)
+	play_sfx.emit(AudioPlayerManager.SFX.ButtonClick)
 	restart_button_pressed.emit()
 
 func _on_close_settings_button_pressed() -> void:
+	play_sfx.emit(AudioPlayerManager.SFX.ButtonClick)
 	settings_ui.hide()
 
 func _on_open_settings_menu_pressed() -> void:
+	play_sfx.emit(AudioPlayerManager.SFX.ButtonClick)
 	settings_ui.show()
 
 func _on_music_volume_slider_value_changed(value: float) -> void:
+	play_sfx.emit(AudioPlayerManager.SFX.ButtonClick)
 	setting_changed.emit(SettingsManager.Settings.BGM_VOLUME, value)
 	
 func _on_sfx_volume_slider_value_changed(value: float) -> void:
+	play_sfx.emit(AudioPlayerManager.SFX.ButtonClick)
 	setting_changed.emit(SettingsManager.Settings.BGM_VOLUME, value)
